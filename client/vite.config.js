73b9be85b56d117adde0287eb34f1fc730b53a2a
Telvin Caesar Varfley <telvinvarfley@gmail.com>
2024-11-08 15:35:22 +0000

    Deploy to Azure Correction

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '../client',  // Ensure Vite works inside the 'client' folder for the build
   plugins: [react()],
   build: {
-    outDir: 'build',  // Specify 'build' as the output directory
+    outDir: './build',  
+    emptyOutDir: true,   // Ensure the output directory is cleaned before building
   },
-})
+});
